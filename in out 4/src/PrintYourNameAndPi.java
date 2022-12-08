93a27273a406a3b052a3645a9d5c49d239eeee79
cp457 <112082288+cp457@users.noreply.github.com>
2022-12-08 14:52:52 +0100

    Add files via upload

---
+++ in out 4/src/PrintYourNameAndPi.java
@@ -0,0 +1,12 @@
+public class PrintYourNameAndPi {
+    public static void main(String[] args) {
+
+        String name = "christian";
+
+        double numberPi = 3.141590;
+
+        System.out.printf("%S", name);
+        System.out.printf("%n", numberPi);
+        System.out.printf("The number Pi is %f", numberPi);
+    }
+}
